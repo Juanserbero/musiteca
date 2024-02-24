b02f7d1e902509daddc89b730bd13e5a8b1ec71e
Juanserbero <javierodriguez607@gmail.com>
2024-02-24 16:34:13 -0500

    MostrarArtista1

--- mtArt.php
+++ mtArt.php
@@ -1,37 +1,80 @@
+<?php
+include './php/conexion.php';
+try {
+    // Consulta para obtener los libros de la tabla 'libro'
+    $sql_leer = 'SELECT id, nombre, fecha, pais, genero FROM artista;';
+    $gsent = $pdo->prepare($sql_leer);
+    $gsent->execute();
+    $resultado = $gsent->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+    exit;
+}
+?>
+
 <html>
-    <head>
-        <title>
-            Musiteca
-        </title>
-    </head>
-    <body>
-        <link rel="stylesheet" href="mtStyle.css">
-        <header>
-            <h1 class="head1">MUSITECA</h1>
-            <nav>
-                <ul>
-                    <li><a href='index.php'>Inicio</a></li>
-                    <li><a href='mtArt.php'>Artista</a></li>
-                    <li><a href='mtAlb.php'>Album</a></li>
-                    <li><a href='mtGen.php'>Género</a></li>
-                </ul>
-                <input type="text" placeholder="Buscar..." class="search">
-            </nav>
-        </header>
-        <section class="mainCont">
-            <section class="lat">
-                <aside>
-                    <h2 class="head2">Artistas</h2>
-                </aside>
-            </section>
-            <section class="artCont">
-                <h3 class="headSelect">ARTISTA X</h3>
-    
-            </section>
+
+<head>
+    <title>
+        Musiteca
+    </title>
+</head>
+
+<body>
+    <link rel="stylesheet" href="mtStyle.css">
+    <header>
+        <h1 class="head1">MUSITECA</h1>
+        <nav>
+            <ul>
+                <li><a href='index.php'>Inicio</a></li>
+                <li><a href='mtArt.php'>Artista</a></li>
+                <li><a href='mtAlb.php'>Album</a></li>
+                <li><a href='mtGen.php'>Género</a></li>
+            </ul>
+            <input type="text" placeholder="Buscar..." class="search">
+        </nav>
+    </header>
+    <section class="mainCont">
+        <section class="lat">
+            <aside>
+                <h2 class="head2">Artistas</h2>
+
+                <?php foreach ($resultado as $r) : ?>
+                    <div class="chapter flex" style="align-items: center;">
+                        <h4 onclick="loadArt(<?php echo $r['id']; ?>)" style="flex-grow: 1; cursor: pointer;"><?php echo $r['nombre']; ?></h4>
+                    </div>
+                <?php endforeach ?>
+
+            </aside>
         </section>
-        <footer>
-            <a href="#" class="SetOrAdm">Ajustes</a>
-            <a href="mtLogin.php" class="SetOrAdm">Administrador</a>
-        </footer>
-    </body>
+        <section class="artCont">
+            <h3 id="nombreArtista"class="headSelect"><?php if ($resultado) {
+                                        echo $resultado[0]['nombre'];
+                                    }    ?>
+            </h3>
+                                    
+
+        </section>
+    </section>
+    <footer>
+        <a href="#" class="SetOrAdm">Ajustes</a>
+        <a href="mtLogin.php" class="SetOrAdm">Administrador</a>
+    </footer>
+</body>
+<script>
+    function loadArt(id) {
+
+        
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("chapter-content").innerHTML = this.responseText;
+                document.getElementById("nombreArtista").innerHTML = nombre;
+            }
+        };
+        xhttp.open("GET", "./php/get_chapter_content.php?id_capitulo=" + id_capitulo, true);
+        xhttp.send();
+    }
+</script>
+
 </html>
